9168f426d0fdebd554c5f87a58367f594ee6b4c3
marek4882 <cwioro200@gmail.com>
2024-06-10 23:37:56 +0200

    Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
-import SignInForm from './pages/SignInForm';
+import SignInForm from "./pages/SignInForm";
+import SignUpForm from "./pages/SignUpForm";
+import ProjectForm from "./pages/ProjectForm";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<SignInForm />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/project" element={<ProjectForm />} />
       </Routes>
     </BrowserRouter>
   );
