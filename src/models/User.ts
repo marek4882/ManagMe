99931cb0f2b3c7e365e178db613d10ef40471f31
marek4882 <cwioro200@gmail.com>
2024-06-10 23:38:36 +0200

    Create Mock

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,3 +4,30 @@ export interface User{
     surname: string;
     role: "Admin" | "Devops" | "Developer"
 }
+
+export const mockUsers: User[] = [
+    {
+      id: "1",
+      name: "Marek",
+      surname: "Ćwioro",
+      role: "Admin"
+    },
+    {
+        id: "2",
+        name: "Jan",
+        surname: "Kowalski",
+        role: "Devops"
+    },
+    {
+        id: "3",
+        name: "Barbara",
+        surname: "Pravi",
+        role: "Developer"
+    },
+    {
+        id: "4",
+        name: "Piotr",
+        surname: "Żyła",
+        role: "Developer"
+    },
+];
