9c65f5545caea226a5356270a5845d89b17f6a24
marek4882 <cwioro200@gmail.com>
2024-06-14 01:08:37 +0200

    .

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,12 +1,12 @@
-export interface Task{
-    id: string;
-    name: string;
-    description: string;
-    priority: "Low" | "Medium" | "High";
-    storyId: string;
-    estimatedTime: number;
-    state: "Todo" | "Doing" | "Done";
-    startDate?: Date | undefined; 
-    endDate?: Date | undefined;
-    userId: string;
+export interface Task {
+  id: string;
+  name: string;
+  description: string;
+  priority: "Low" | "Medium" | "High";
+  storyId: string;
+  estimatedTime: number;
+  state: "Todo" | "Doing" | "Done";
+  startDate?: Date | undefined;
+  endDate?: Date | undefined;
+  assigneeId: string;
 }
