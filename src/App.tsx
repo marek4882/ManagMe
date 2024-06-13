011f60b3255425588a6e9a4d98eb6d7241c9b4ec
marek4882 <cwioro200@gmail.com>
2024-06-14 01:07:58 +0200

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import NavBar from "./components/NavBar";
 import SignInForm from "./pages/SignInForm";
 import SignUpForm from "./pages/SignUpForm";
 import ProjectForm from "./pages/ProjectForm";
+import StoryForm from "./pages/StoryForm";
+import TaskForm from "./pages/TaskForm";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/login" element={<SignInForm />} />
         <Route path="/signup" element={<SignUpForm />} />
         <Route path="/project" element={<ProjectForm />} />
+        <Route path="/project/:projectId/story" element={<StoryForm />} />
+        <Route path="/story/:storyId/task" element={<TaskForm />} />
       </Routes>
     </BrowserRouter>
   );
