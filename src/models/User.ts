a1ac3727ab41e1527e8b87fab88621792bd9fde8
marek4882 <cwioro200@gmail.com>
2024-06-14 01:08:53 +0200

    change MockUsers

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,33 +1,43 @@
-export interface User{
-    id: string;
-    name: string;
-    surname: string;
-    role: "Admin" | "Devops" | "Developer"
+export interface User {
+  id: string;
+  name: string;
+  surname: string;
+  role: "Admin" | "Devops" | "Developer";
+  login: string;
+  password: string;
 }
 
 export const mockUsers: User[] = [
-    {
-      id: "1",
-      name: "Marek",
-      surname: "Ćwioro",
-      role: "Admin"
-    },
-    {
-        id: "2",
-        name: "Jan",
-        surname: "Kowalski",
-        role: "Devops"
-    },
-    {
-        id: "3",
-        name: "Barbara",
-        surname: "Pravi",
-        role: "Developer"
-    },
-    {
-        id: "4",
-        name: "Piotr",
-        surname: "Żyła",
-        role: "Developer"
-    },
+  {
+    id: "1",
+    name: "Marek",
+    surname: "Ćwioro",
+    role: "Admin",
+    login: "user1",
+    password: "password1",
+  },
+  {
+    id: "2",
+    name: "Jan",
+    surname: "Kowalski",
+    role: "Devops",
+    login: "user2",
+    password: "password2",
+  },
+  {
+    id: "3",
+    name: "Barbara",
+    surname: "Pravi",
+    role: "Developer",
+    login: "user3",
+    password: "password3",
+  },
+  {
+    id: "4",
+    name: "Piotr",
+    surname: "Żyła",
+    role: "Developer",
+    login: "user4",
+    password: "password4",
+  },
 ];
