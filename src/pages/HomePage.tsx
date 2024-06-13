865f5583bd36c871bfcb6a098daffbfd471fdf5d
marek4882 <cwioro200@gmail.com>
2024-06-14 01:09:08 +0200

    .

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import Button from "../components/Button";
-import heroImage from "../assets/hero-image.svg"; 
+import heroImage from "../assets/hero-image.svg";
 
 function HomePage() {
   return (
